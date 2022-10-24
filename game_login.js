7c8f6a647accd043d3e7100323191562e6a0b3ed
RW773 <111027857+RW773@users.noreply.github.com>
2022-10-23 19:37:53 -0500

    Delete game_login.js

--- game_login.js
+++
@@ -1,9 +0,0 @@
-// Create function addUser()
-function ____ {
-  // Get value of user by id player1_name_input and player2_name_input
-
-  // Store these values locally
-
-  //Assign "game_page.html" to window.location
-}
-
